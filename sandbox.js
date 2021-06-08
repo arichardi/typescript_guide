a7ae8c8bcdb5065fc19b65e9fbf7492781484d22
Andre Richardi <a.richardi@gmail.com>
2021-06-08 09:47:42 -0300

    first commit
    qith '#' will be ignored, and an empty message aborts the commit.
    q

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var character = 'Luigi';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (element) { return console.log(element); });
---
+++ sandbox.ts
@@ -0,0 +1,7 @@
+const character = 'Luigi'
+
+console.log(character)
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach( element => console.log(element))
